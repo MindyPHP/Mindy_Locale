731849d2ff0c846fb836464fe3e32b68f7a18e64
Falaleev Maxim <max@studio107.ru>
2014-10-24 10:59:04 +0400

    Add validation dict

---
+++ src/Mindy/Locale/messages/ru/validation.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * All rights reserved.
+ * 
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 23/10/14.10.2014 18:22
+ */
+
+return [
+    'Is not a valid email address' => 'Не корректный адрес электронной почты'
+];
